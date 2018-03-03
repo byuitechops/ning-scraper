680230c680fa9a0edf5df9e548b27b0b67374ed9
Earl <benjam55@byui.edu>
2018-03-02 17:22:28 -0700

    apparently there were quite a few things wrong with my code, so it should be more cleaned up now

--- scrape.js
+++ scrape.js
@@ -1,51 +1,55 @@
 var got = require('got');
 var fs = require('fs');
 var cheerio = require('cheerio')
+var moment = require('moment')
 
 async function getBlogLinks(file){
-    async function forEach(forum,pagenum){
-        try{
-            var r = await got(`https://onlineinstruction.ning.com/${forum}?page=${pagenum}`)
-            console.log(forum,pagenum)
-            var $ = cheerio.load(r.body)
-            return $('.entry-title a').get().map(n => n.attribs.href)
-        } catch(e){
-            console.error(e)
-            return []
-        }
+    async function forEach(forum){
+        var pagenum = 1
+        var isDone
+        var hrefs = []
+        do{
+            try{
+                console.log(forum,pagenum)
+                var r = await got(`https://onlineinstruction.ning.com/${forum}?page=${pagenum}`)
+                var $ = cheerio.load(r.body)
+                isDone = $('li:last-child .pagination-current').length
+                hrefs.push(...$('.entry-title a').get().map(n => n.attribs.href))
+                pagenum++
+            } catch(e){
+                console.error(e)
+            }
+        } while(!isDone)
+        return hrefs
     }
 
     var forums = [
-        {name:'blogs',pagecount:36},
-        {name:'working-with-students',pagecount:15},
-        {name:'working-in-the-online-classroom',pagecount:12},
-        {name:'technology-resources',pagecount:6},
-        {name:'i-learn-3-0-forum',pagecount:66},
-        {name:'contract-and-employee-questions',pagecount:5},
-        {name:'online-community-discussions',pagecount:9},
+        'blogs',
+        'working-with-students',
+        'working-in-the-online-classroom',
+        'technology-resources',
+        'i-learn-3-0-forum',
+        'contract-and-employee-questions',
+        'online-community-discussions',
     ]
-    var hrefs = []
-    for(var f = 0; f < forums.length; f++){
-        var pages = Array(forums[f].pagecount).fill().map((n,i) => i+1)
-        var chunk = await Promise.all(pages.map(n => forEach(forums[f].name,n)))
-        hrefs.push(...[].concat(...chunk))
-    }
+    var hrefs = [].concat(...await Promise.all(forums.map(forEach)))
     fs.writeFileSync(file,JSON.stringify(hrefs))
     return hrefs
 }
 
-async function scrapePost(infile,outfile){
+async function scrapePost(infile,outfile,limit){
     async function forEach(href){
         try{
             var r = await got(href)
             var $ = cheerio.load(r.body)
-            var parseTime = (m,mon,day,year,time) => ({month:mon,day:day,year:year,time:time})
+            var parseTime = (m,mon,day,year,time) => moment(`${mon} ${day} ${year} ${time}`,'MMMM DD YYYY h:mma').format()
             return {
                 url:href,
-                catagories: $('.subnav a[href]').get().map(n => $(n).text()).slice(1),
+                forum:href.match(/com\/(.*?)\//)[1],
+                labels: $('.subnav a[href]').get().map(n => $(n).text()).slice(1),
                 tags:$('.entry-tags a').get().map(n => $(n).text()),
                 id:$('[data-content-id]').attr('data-content-id'),
-                title:$('.entry-title a').text(),
+                subject:$('.entry-title a').text(),
                 author:{
                     name:$('.entry-byline a:first-child').text(),
                     link: $('.entry-byline a').attr('href'),
@@ -59,12 +63,12 @@ async function scrapePost(infile,outfile){
         }
     }
 
-    var hrefs = JSON.parse(fs.readFileSync(infile,'utf-8'))
+    var hrefs = JSON.parse(fs.readFileSync(infile,'utf-8')).slice(-limit)
     var jumpSize = 20
     var blogs = []
     for(var i = 0; i < hrefs.length; i+=jumpSize){
         console.log(i,i+jumpSize)
-        var pages = Array(jumpSize).fill().map((n,d) => hrefs[Math.min(hrefs.length-1,d+i)])
+        var pages = Array(Math.min(hrefs.length-i,jumpSize)).fill().map((n,d) => hrefs[d+i])
         var chunk = await Promise.all(pages.map(forEach))
         blogs.push(...chunk.filter(n => n))
     }
@@ -72,21 +76,21 @@ async function scrapePost(infile,outfile){
     return blogs
 }
 
-async function scrapeComments(infile,outfile){
+async function scrapeComments(infile,outfile,limit){
     async function forEach(blogid){
         try{
             var r = await got(`https://onlineinstruction.ning.com/main/comment/list?id=${blogid}&count=100000000000000`)
             var $ = cheerio.load(JSON.parse(r.body).commentsMarkup)
             var scrape = $li => ({
                 id:$li.attr('data-comment-id'),
-                time:$li.attr('data-comment-created-date'),
+                time:moment($li.attr('data-comment-created-date')).format(),
                 author:{
                     name:$li.find('> div .comments-author-name').text(),
                     link:$li.find('> div .comments-author-name').attr('href'),
                     photo:$li.find('> div .photo.avatar').attr('src'),
                 },
                 body:$li.find('> div .entry-content').html(),
-                replies:$li.find('ul li').get().map(li => scrape($(li)))
+                replies:$li.find('> ul > li').get().map(li => scrape($(li)))
             })
             return {
                 blogid:blogid,
@@ -97,12 +101,12 @@ async function scrapeComments(infile,outfile){
         }
     }
 
-    var blogs = JSON.parse(fs.readFileSync(infile,'utf-8'))
+    var blogs = JSON.parse(fs.readFileSync(infile,'utf-8')).slice(-limit)
     var jumpSize = 10
     var comments = []
     for(var i = 0; i < blogs.length; i+=jumpSize){
         console.log(i,i+jumpSize)
-        var pages = Array(jumpSize).fill().filter((n,d) => blogs[d+i]).map((n,d) => blogs[Math.min(blogs.length-1,d+i)].id)
+        var pages = Array(Math.min(blogs.length-i,jumpSize)).fill().filter((n,d) => blogs[d+i]).map((n,d) => blogs[d+i].id)
         var chunk = await Promise.all(pages.map(forEach))
         comments.push(...chunk.filter(n => n))
     }
@@ -110,14 +114,25 @@ async function scrapeComments(infile,outfile){
     return comments
 }
 
+async function join(blogsfile,commentsfile,outfile){
+    var blogs = require(blogsfile)
+    var comments = require(commentsfile)
+    var blogmap = blogs.reduce((obj,blog) => {obj[blog.id] = blog;return obj},{})
+    comments.forEach(comment => {
+        blogmap[comment.blogid].comments = comment.comments
+    })
+    fs.writeFileSync(outfile,JSON.stringify(blogs))
+}
 
 async function main(){
     var hrefsfile = './data/hrefs.json'
     var blogsfile = './data/blogs.json'
     var commentsfile = './data/comments.json'
+    var allfile = './data/joined.json'
     
     // await getBlogLinks(hrefsfile)
-    // await scrapePost(hrefsfile,blogsfile)
-    // await scrapeComments(blogsfile,commentsfile)
+    await scrapePost(hrefsfile,blogsfile)
+    await scrapeComments(blogsfile,commentsfile)
+    await join(blogsfile,commentsfile,allfile)
 }
 main()
